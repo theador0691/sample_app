6b72500b1d42cc33632fe18f0088f7e8da3a5c3c
Andy Taylor <Theador0691@gmail.com>
2015-10-02 20:40:43 +0100

    Make a basic user modle including secure password and the tests to go with them

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,10 +13,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select "title", full_title("Sign up")
   end
-
-
-
-
   # test "the truth" do
   #   assert true
   # end
